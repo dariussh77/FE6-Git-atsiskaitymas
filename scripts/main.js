651ab02f0815f6354c2935c271b4bdafc7592a59
dariussh77 <dariussh77@gmail.com>
2023-04-18 17:56:02 +0300

    Sudėtas visų likusių tabų funkcionalumas

--- scripts/main.js
+++ scripts/main.js
@@ -39,3 +39,120 @@ qIntro.addEventListener('click',()=>{
         qbuttons.classList.remove('displayNone');
     })
 });
+let qWork=document.querySelector('#work');
+qWork.addEventListener('click',()=>{
+    let qhero=document.querySelector('#hero');
+    qhero.classList.add('displayNone');
+    let qbuttons=document.querySelector('#buttons');
+    qbuttons.classList.add('displayNone');
+
+    let qtabs=document.querySelector('#tabs');
+    let div=document.createElement('div');
+    let img=document.createElement('img');
+    img.setAttribute('src','./resources/desktoptab.jpg');
+    img.setAttribute('alt', 'Kosminė stotis');
+    let h1=document.createElement('h1');
+    let h1Text=document.createTextNode('Work');
+    h1.appendChild(h1Text);
+    let p1=document.createElement('p');
+    let p1Text=document.createTextNode(text1);
+    p1.appendChild(p1Text);
+    let p2=document.createElement('p');
+    let p2Text=document.createTextNode(text2);
+    p2.appendChild(p2Text);
+    let xButton=document.createElement('div');
+    xButton.classList.add('x');
+    let xImg=document.createElement('img');
+    xImg.setAttribute('src','./resources/xmark-solid.svg');
+    xImg.setAttribute('alt','X mygtukas');
+    xButton.appendChild(xImg);
+
+    div.append(h1,img,p1,p2,xButton);
+    qtabs.appendChild(div);
+
+    let qXbutton=document.querySelector('.x');
+    qXbutton.addEventListener('click',()=>{
+        let qtabsDiv=document.querySelector('#tabs>div');
+        qtabsDiv.remove();
+        qhero.classList.remove('displayNone');
+        qbuttons.classList.remove('displayNone');
+    })
+});
+let qAbout=document.querySelector('#about');
+qAbout.addEventListener('click',()=>{
+    let qhero=document.querySelector('#hero');
+    qhero.classList.add('displayNone');
+    let qbuttons=document.querySelector('#buttons');
+    qbuttons.classList.add('displayNone');
+
+    let qtabs=document.querySelector('#tabs');
+    let div=document.createElement('div');
+    let img=document.createElement('img');
+    img.setAttribute('src','./resources/desktoptab.jpg');
+    img.setAttribute('alt', 'Kosminė stotis');
+    let h1=document.createElement('h1');
+    let h1Text=document.createTextNode('About');
+    h1.appendChild(h1Text);
+    let p1=document.createElement('p');
+    let p1Text=document.createTextNode(text1);
+    p1.appendChild(p1Text);
+    let p2=document.createElement('p');
+    let p2Text=document.createTextNode(text2);
+    p2.appendChild(p2Text);
+    let xButton=document.createElement('div');
+    xButton.classList.add('x');
+    let xImg=document.createElement('img');
+    xImg.setAttribute('src','./resources/xmark-solid.svg');
+    xImg.setAttribute('alt','X mygtukas');
+    xButton.appendChild(xImg);
+
+    div.append(h1,img,p1,p2,xButton);
+    qtabs.appendChild(div);
+
+    let qXbutton=document.querySelector('.x');
+    qXbutton.addEventListener('click',()=>{
+        let qtabsDiv=document.querySelector('#tabs>div');
+        qtabsDiv.remove();
+        qhero.classList.remove('displayNone');
+        qbuttons.classList.remove('displayNone');
+    })
+});
+let qContacts=document.querySelector('#contacts');
+qContacts.addEventListener('click',()=>{
+    let qhero=document.querySelector('#hero');
+    qhero.classList.add('displayNone');
+    let qbuttons=document.querySelector('#buttons');
+    qbuttons.classList.add('displayNone');
+
+    let qtabs=document.querySelector('#tabs');
+    let div=document.createElement('div');
+    let img=document.createElement('img');
+    img.setAttribute('src','./resources/desktoptab.jpg');
+    img.setAttribute('alt', 'Kosminė stotis');
+    let h1=document.createElement('h1');
+    let h1Text=document.createTextNode('Contacts');
+    h1.appendChild(h1Text);
+    let p1=document.createElement('p');
+    let p1Text=document.createTextNode(text1);
+    p1.appendChild(p1Text);
+    let p2=document.createElement('p');
+    let p2Text=document.createTextNode(text2);
+    p2.appendChild(p2Text);
+    let xButton=document.createElement('div');
+    xButton.classList.add('x');
+    let xImg=document.createElement('img');
+    xImg.setAttribute('src','./resources/xmark-solid.svg');
+    xImg.setAttribute('alt','X mygtukas');
+    xButton.appendChild(xImg);
+
+    div.append(h1,img,p1,p2,xButton);
+    qtabs.appendChild(div);
+
+    let qXbutton=document.querySelector('.x');
+    qXbutton.addEventListener('click',()=>{
+        let qtabsDiv=document.querySelector('#tabs>div');
+        qtabsDiv.remove();
+        qhero.classList.remove('displayNone');
+        qbuttons.classList.remove('displayNone');
+    })
+});
